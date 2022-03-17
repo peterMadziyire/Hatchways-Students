af5a914dbc8a11b5520fc67d866aee78660b2779
peterMadziyire <90290219+peterMadziyire@users.noreply.github.com>
2022-03-17 22:15:44 +0000

    Fix bugs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+
 import React, {useState, useEffect} from "react";
 
 import './App.css';
 import Spinner from 'react-bootstrap/Spinner';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 import StudentCard from './components/studentCard';
 
 function App() {
